553bdfb9a5f00cdf3d75a54f96c8adbdef93285e
AleSantiagoAM <alessandroalarez563@gmail.com>
2025-05-18 16:14:22 -0600

    Add files via upload

---
+++ JavaSQL/src/main/java/com/mycompany/javasql/JavaSQL.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javasql;
+
+/**
+ *
+ * @author aless
+ */
+public class JavaSQL {
+
+    public static void main(String[] args) {
+        Formulario.Interfaz objF= new Formulario.Interfaz();
+        objF.setVisible(true);
+    }
+}
